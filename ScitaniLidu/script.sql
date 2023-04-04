cf644ff25034ca031ba96ab665738e67552adf59
Michal-Sivicek <73881006+Michal-Sivicek@users.noreply.github.com>
2023-04-04 13:25:46 +0200

    Add new table in script.sql

--- ScitaniLidu/script.sql
+++ ScitaniLidu/script.sql
@@ -15,6 +15,24 @@ CREATE TABLE users (
 COMMIT;
 
 BEGIN;
+CREATE TABLE users_info (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    jmeno VARCHAR(50) NOT NULL,
+    prijmeni VARCHAR(50) NOT NULL,
+    bydliste VARCHAR(100) NOT NULL,
+    nabozenstvi BOOL NOT NULL,
+    telefonni_cislo VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    narodnost VARCHAR(50) NOT NULL,
+    statni_obcanstvi VARCHAR(50) NOT NULL,
+    vzdelani ENUM('zakladni skola', 'stredni skola', 'vysoka skola') NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+COMMIT;
+
+
+BEGIN;
 INSERT INTO `loginscitanilidu`.`users` (`id`, `username`, `password`, `role`) VALUES ('1', 'msiv', 'msiv1', 'admin');
 INSERT INTO `loginscitanilidu`.`users` (`username`, `password`, `role`) VALUES ('uzivatel', 'uzivatel1', 'normal');
 COMMIT;
