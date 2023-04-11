e561df264575eed684fca86dd00adf220be52f9b
Michal-Sivicek <73881006+Michal-Sivicek@users.noreply.github.com>
2023-04-11 12:39:44 +0200

    Added forms to UserHousesPage

--- ScitaniLidu/UserHousesPage.Designer.cs
+++ ScitaniLidu/UserHousesPage.Designer.cs
@@ -29,20 +29,247 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.obec = new System.Windows.Forms.Label();
+            this.textBoxObec = new System.Windows.Forms.TextBox();
+            this.numberHouse = new System.Windows.Forms.Label();
+            this.textBoxNumberHouse = new System.Windows.Forms.TextBox();
+            this.adress = new System.Windows.Forms.Label();
+            this.textBoxAdress = new System.Windows.Forms.TextBox();
+            this.PSC = new System.Windows.Forms.Label();
+            this.textBoxPSC = new System.Windows.Forms.TextBox();
+            this.obydlenostDomu = new System.Windows.Forms.Label();
+            this.textBoxObydlenostDomu = new System.Windows.Forms.TextBox();
+            this.rokVystavbyReknostrukce = new System.Windows.Forms.Label();
+            this.textBoxVystavbaRekonstrukce = new System.Windows.Forms.TextBox();
+            this.materialZdi = new System.Windows.Forms.Label();
+            this.textBoxMaterialZdi = new System.Windows.Forms.TextBox();
+            this.pocetPodlazi = new System.Windows.Forms.Label();
+            this.textBoxPocetPodlazi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBoxGDPR = new System.Windows.Forms.CheckBox();
+            this.linkLabelGDPR = new System.Windows.Forms.LinkLabel();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(276, 114);
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Location = new System.Drawing.Point(287, 39);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(220, 23);
+            this.textBox1.Size = new System.Drawing.Size(298, 23);
             this.textBox1.TabIndex = 0;
+            // 
+            // labelName
+            // 
+            this.labelName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(138, 42);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(143, 15);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "Jmeno a příjmení majitele";
+            // 
+            // obec
+            // 
+            this.obec.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.obec.AutoSize = true;
+            this.obec.Location = new System.Drawing.Point(246, 71);
+            this.obec.Name = "obec";
+            this.obec.Size = new System.Drawing.Size(35, 15);
+            this.obec.TabIndex = 2;
+            this.obec.Text = "Obec";
+            // 
+            // textBoxObec
+            // 
+            this.textBoxObec.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxObec.Location = new System.Drawing.Point(287, 68);
+            this.textBoxObec.Name = "textBoxObec";
+            this.textBoxObec.Size = new System.Drawing.Size(298, 23);
+            this.textBoxObec.TabIndex = 3;
+            // 
+            // numberHouse
+            // 
+            this.numberHouse.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.numberHouse.AutoSize = true;
+            this.numberHouse.Location = new System.Drawing.Point(213, 100);
+            this.numberHouse.Name = "numberHouse";
+            this.numberHouse.Size = new System.Drawing.Size(68, 15);
+            this.numberHouse.TabIndex = 4;
+            this.numberHouse.Text = "Číslo domu";
+            // 
+            // textBoxNumberHouse
+            // 
+            this.textBoxNumberHouse.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxNumberHouse.Location = new System.Drawing.Point(287, 97);
+            this.textBoxNumberHouse.Name = "textBoxNumberHouse";
+            this.textBoxNumberHouse.Size = new System.Drawing.Size(298, 23);
+            this.textBoxNumberHouse.TabIndex = 5;
+            // 
+            // adress
+            // 
+            this.adress.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.adress.AutoSize = true;
+            this.adress.Location = new System.Drawing.Point(248, 126);
+            this.adress.Name = "adress";
+            this.adress.Size = new System.Drawing.Size(33, 15);
+            this.adress.TabIndex = 6;
+            this.adress.Text = "Ulice";
+            // 
+            // textBoxAdress
+            // 
+            this.textBoxAdress.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxAdress.Location = new System.Drawing.Point(287, 126);
+            this.textBoxAdress.Name = "textBoxAdress";
+            this.textBoxAdress.Size = new System.Drawing.Size(298, 23);
+            this.textBoxAdress.TabIndex = 7;
+            // 
+            // PSC
+            // 
+            this.PSC.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PSC.AutoSize = true;
+            this.PSC.Location = new System.Drawing.Point(253, 158);
+            this.PSC.Name = "PSC";
+            this.PSC.Size = new System.Drawing.Size(28, 15);
+            this.PSC.TabIndex = 8;
+            this.PSC.Text = "PSČ";
+            // 
+            // textBoxPSC
+            // 
+            this.textBoxPSC.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxPSC.Location = new System.Drawing.Point(287, 155);
+            this.textBoxPSC.Name = "textBoxPSC";
+            this.textBoxPSC.Size = new System.Drawing.Size(298, 23);
+            this.textBoxPSC.TabIndex = 9;
+            // 
+            // obydlenostDomu
+            // 
+            this.obydlenostDomu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.obydlenostDomu.AutoSize = true;
+            this.obydlenostDomu.Location = new System.Drawing.Point(178, 187);
+            this.obydlenostDomu.Name = "obydlenostDomu";
+            this.obydlenostDomu.Size = new System.Drawing.Size(103, 15);
+            this.obydlenostDomu.TabIndex = 10;
+            this.obydlenostDomu.Text = "Obydlenost domu";
+            // 
+            // textBoxObydlenostDomu
+            // 
+            this.textBoxObydlenostDomu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxObydlenostDomu.Location = new System.Drawing.Point(287, 184);
+            this.textBoxObydlenostDomu.Name = "textBoxObydlenostDomu";
+            this.textBoxObydlenostDomu.Size = new System.Drawing.Size(298, 23);
+            this.textBoxObydlenostDomu.TabIndex = 11;
+            // 
+            // rokVystavbyReknostrukce
+            // 
+            this.rokVystavbyReknostrukce.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.rokVystavbyReknostrukce.AutoSize = true;
+            this.rokVystavbyReknostrukce.Location = new System.Drawing.Point(171, 206);
+            this.rokVystavbyReknostrukce.Name = "rokVystavbyReknostrukce";
+            this.rokVystavbyReknostrukce.Size = new System.Drawing.Size(110, 30);
+            this.rokVystavbyReknostrukce.TabIndex = 12;
+            this.rokVystavbyReknostrukce.Text = "Rok výstavby nebo \r\nrekonstrukce domu";
+            // 
+            // textBoxVystavbaRekonstrukce
+            // 
+            this.textBoxVystavbaRekonstrukce.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxVystavbaRekonstrukce.Location = new System.Drawing.Point(287, 213);
+            this.textBoxVystavbaRekonstrukce.Name = "textBoxVystavbaRekonstrukce";
+            this.textBoxVystavbaRekonstrukce.Size = new System.Drawing.Size(298, 23);
+            this.textBoxVystavbaRekonstrukce.TabIndex = 13;
+            // 
+            // materialZdi
+            // 
+            this.materialZdi.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.materialZdi.AutoSize = true;
+            this.materialZdi.Location = new System.Drawing.Point(165, 245);
+            this.materialZdi.Name = "materialZdi";
+            this.materialZdi.Size = new System.Drawing.Size(116, 15);
+            this.materialZdi.TabIndex = 14;
+            this.materialZdi.Text = "Materiál nosných zdí";
+            // 
+            // textBoxMaterialZdi
+            // 
+            this.textBoxMaterialZdi.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxMaterialZdi.Location = new System.Drawing.Point(287, 242);
+            this.textBoxMaterialZdi.Name = "textBoxMaterialZdi";
+            this.textBoxMaterialZdi.Size = new System.Drawing.Size(298, 23);
+            this.textBoxMaterialZdi.TabIndex = 15;
+            // 
+            // pocetPodlazi
+            // 
+            this.pocetPodlazi.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pocetPodlazi.AutoSize = true;
+            this.pocetPodlazi.Location = new System.Drawing.Point(203, 274);
+            this.pocetPodlazi.Name = "pocetPodlazi";
+            this.pocetPodlazi.Size = new System.Drawing.Size(78, 15);
+            this.pocetPodlazi.TabIndex = 16;
+            this.pocetPodlazi.Text = "Počet podlaží";
+            // 
+            // textBoxPocetPodlazi
+            // 
+            this.textBoxPocetPodlazi.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxPocetPodlazi.Location = new System.Drawing.Point(287, 271);
+            this.textBoxPocetPodlazi.Name = "textBoxPocetPodlazi";
+            this.textBoxPocetPodlazi.Size = new System.Drawing.Size(298, 23);
+            this.textBoxPocetPodlazi.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(510, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Odeslat";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxGDPR
+            // 
+            this.checkBoxGDPR.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.checkBoxGDPR.AutoSize = true;
+            this.checkBoxGDPR.Location = new System.Drawing.Point(287, 300);
+            this.checkBoxGDPR.Name = "checkBoxGDPR";
+            this.checkBoxGDPR.Size = new System.Drawing.Size(291, 34);
+            this.checkBoxGDPR.TabIndex = 19;
+            this.checkBoxGDPR.Text = "Souhlasím s podmínkami ochrana osobních údajů\r\n\r\n";
+            this.checkBoxGDPR.UseVisualStyleBackColor = true;
+            // 
+            // linkLabelGDPR
+            // 
+            this.linkLabelGDPR.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.linkLabelGDPR.AutoSize = true;
+            this.linkLabelGDPR.Location = new System.Drawing.Point(305, 319);
+            this.linkLabelGDPR.Name = "linkLabelGDPR";
+            this.linkLabelGDPR.Size = new System.Drawing.Size(37, 15);
+            this.linkLabelGDPR.TabIndex = 20;
+            this.linkLabelGDPR.TabStop = true;
+            this.linkLabelGDPR.Text = "GDPR";
             // 
             // UserHousesPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.linkLabelGDPR);
+            this.Controls.Add(this.checkBoxGDPR);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxPocetPodlazi);
+            this.Controls.Add(this.pocetPodlazi);
+            this.Controls.Add(this.textBoxMaterialZdi);
+            this.Controls.Add(this.materialZdi);
+            this.Controls.Add(this.textBoxVystavbaRekonstrukce);
+            this.Controls.Add(this.rokVystavbyReknostrukce);
+            this.Controls.Add(this.textBoxObydlenostDomu);
+            this.Controls.Add(this.obydlenostDomu);
+            this.Controls.Add(this.textBoxPSC);
+            this.Controls.Add(this.PSC);
+            this.Controls.Add(this.textBoxAdress);
+            this.Controls.Add(this.adress);
+            this.Controls.Add(this.textBoxNumberHouse);
+            this.Controls.Add(this.numberHouse);
+            this.Controls.Add(this.textBoxObec);
+            this.Controls.Add(this.obec);
+            this.Controls.Add(this.labelName);
             this.Controls.Add(this.textBox1);
             this.Name = "UserHousesPage";
             this.Text = "UserHousesPage";
@@ -54,5 +281,25 @@
         #endregion
 
         private TextBox textBox1;
+        private Label labelName;
+        private Label obec;
+        private TextBox textBoxObec;
+        private Label numberHouse;
+        private TextBox textBoxNumberHouse;
+        private Label adress;
+        private TextBox textBoxAdress;
+        private Label PSC;
+        private TextBox textBoxPSC;
+        private Label obydlenostDomu;
+        private TextBox textBoxObydlenostDomu;
+        private Label rokVystavbyReknostrukce;
+        private TextBox textBoxVystavbaRekonstrukce;
+        private Label materialZdi;
+        private TextBox textBoxMaterialZdi;
+        private Label pocetPodlazi;
+        private TextBox textBoxPocetPodlazi;
+        private Button button1;
+        private CheckBox checkBoxGDPR;
+        private LinkLabel linkLabelGDPR;
     }
 }
--- ScitaniLidu/UserPopulationPage.Designer.cs
+++ ScitaniLidu/UserPopulationPage.Designer.cs
@@ -49,177 +49,225 @@
             this.linkLabelGDPR = new System.Windows.Forms.LinkLabel();
             this.LabelEdjucation = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
             // usernameTextBox
+            // 
             this.usernameTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.usernameTextBox.Location = new System.Drawing.Point(270, 51);
+            this.usernameTextBox.Location = new System.Drawing.Point(270, 36);
             this.usernameTextBox.Name = "usernameTextBox";
             this.usernameTextBox.Size = new System.Drawing.Size(308, 23);
             this.usernameTextBox.TabIndex = 0;
+            // 
             // username
+            // 
             this.username.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.username.AutoSize = true;
-            this.username.Location = new System.Drawing.Point(222, 54);
+            this.username.Location = new System.Drawing.Point(222, 39);
             this.username.Name = "username";
             this.username.Size = new System.Drawing.Size(42, 15);
             this.username.TabIndex = 1;
             this.username.Text = "Jmeno";
+            // 
             // lastname
+            // 
             this.lastname.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.lastname.AutoSize = true;
-            this.lastname.Location = new System.Drawing.Point(213, 83);
+            this.lastname.Location = new System.Drawing.Point(213, 68);
             this.lastname.Name = "lastname";
             this.lastname.Size = new System.Drawing.Size(51, 15);
             this.lastname.TabIndex = 2;
             this.lastname.Text = "Příjmení";
+            // 
             // lastnameTextBox
+            // 
             this.lastnameTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lastnameTextBox.Location = new System.Drawing.Point(270, 80);
+            this.lastnameTextBox.Location = new System.Drawing.Point(270, 65);
             this.lastnameTextBox.Name = "lastnameTextBox";
             this.lastnameTextBox.Size = new System.Drawing.Size(308, 23);
             this.lastnameTextBox.TabIndex = 3;
+            // 
             // adressTextBox
+            // 
             this.adressTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.adressTextBox.Location = new System.Drawing.Point(270, 108);
+            this.adressTextBox.Location = new System.Drawing.Point(270, 94);
             this.adressTextBox.Name = "adressTextBox";
             this.adressTextBox.Size = new System.Drawing.Size(308, 23);
             this.adressTextBox.TabIndex = 4;
+            // 
             // label1
+            // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(216, 112);
+            this.label1.Location = new System.Drawing.Point(216, 97);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 15);
             this.label1.TabIndex = 5;
             this.label1.Text = "Bydliště";
+            // 
             // textBoxNabozenstvi
+            // 
             this.textBoxNabozenstvi.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.textBoxNabozenstvi.AutoSize = true;
-            this.textBoxNabozenstvi.Location = new System.Drawing.Point(192, 140);
+            this.textBoxNabozenstvi.Location = new System.Drawing.Point(192, 126);
             this.textBoxNabozenstvi.Name = "textBoxNabozenstvi";
             this.textBoxNabozenstvi.Size = new System.Drawing.Size(72, 15);
             this.textBoxNabozenstvi.TabIndex = 7;
             this.textBoxNabozenstvi.Text = "Náboženství";
+            // 
             // label2
+            // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(234, 224);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 15);
             this.label2.TabIndex = 10;
+            // 
             // phoneNumber
+            // 
             this.phoneNumber.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.phoneNumber.AutoSize = true;
-            this.phoneNumber.Location = new System.Drawing.Point(211, 169);
+            this.phoneNumber.Location = new System.Drawing.Point(211, 155);
             this.phoneNumber.Name = "phoneNumber";
             this.phoneNumber.Size = new System.Drawing.Size(53, 15);
             this.phoneNumber.TabIndex = 11;
             this.phoneNumber.Text = "Tel. Číslo";
+            // 
             // textBoxPhoneNumber
+            // 
             this.textBoxPhoneNumber.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBoxPhoneNumber.Location = new System.Drawing.Point(270, 166);
+            this.textBoxPhoneNumber.Location = new System.Drawing.Point(270, 152);
             this.textBoxPhoneNumber.Name = "textBoxPhoneNumber";
             this.textBoxPhoneNumber.Size = new System.Drawing.Size(308, 23);
             this.textBoxPhoneNumber.TabIndex = 12;
+            // 
             // email
+            // 
             this.email.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.email.AutoSize = true;
-            this.email.Location = new System.Drawing.Point(228, 195);
+            this.email.Location = new System.Drawing.Point(228, 184);
             this.email.Name = "email";
             this.email.Size = new System.Drawing.Size(36, 15);
             this.email.TabIndex = 13;
             this.email.Text = "Email";
+            // 
             // textBoxEmail
+            // 
             this.textBoxEmail.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBoxEmail.Location = new System.Drawing.Point(270, 195);
+            this.textBoxEmail.Location = new System.Drawing.Point(270, 181);
             this.textBoxEmail.Name = "textBoxEmail";
             this.textBoxEmail.Size = new System.Drawing.Size(308, 23);
             this.textBoxEmail.TabIndex = 14;
+            // 
             // nationality
+            // 
             this.nationality.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.nationality.AutoSize = true;
-            this.nationality.Location = new System.Drawing.Point(201, 224);
+            this.nationality.Location = new System.Drawing.Point(201, 213);
             this.nationality.Name = "nationality";
             this.nationality.Size = new System.Drawing.Size(63, 15);
             this.nationality.TabIndex = 15;
             this.nationality.Text = "Národnost";
+            // 
             // textBoxNationality
+            // 
             this.textBoxNationality.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBoxNationality.Location = new System.Drawing.Point(270, 224);
+            this.textBoxNationality.Location = new System.Drawing.Point(270, 210);
             this.textBoxNationality.Name = "textBoxNationality";
             this.textBoxNationality.Size = new System.Drawing.Size(308, 23);
             this.textBoxNationality.TabIndex = 16;
+            // 
             // citizenShip
+            // 
             this.citizenShip.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.citizenShip.AutoSize = true;
-            this.citizenShip.Location = new System.Drawing.Point(171, 256);
+            this.citizenShip.Location = new System.Drawing.Point(171, 242);
             this.citizenShip.Name = "citizenShip";
             this.citizenShip.Size = new System.Drawing.Size(93, 15);
             this.citizenShip.TabIndex = 17;
             this.citizenShip.Text = "Státní Občanství";
+            // 
             // textBoxCitizenShip
+            // 
             this.textBoxCitizenShip.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBoxCitizenShip.Location = new System.Drawing.Point(270, 253);
+            this.textBoxCitizenShip.Location = new System.Drawing.Point(270, 239);
             this.textBoxCitizenShip.Name = "textBoxCitizenShip";
             this.textBoxCitizenShip.Size = new System.Drawing.Size(308, 23);
             this.textBoxCitizenShip.TabIndex = 18;
+            // 
             // edjucation
+            // 
             this.edjucation.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.edjucation.AutoSize = true;
-            this.edjucation.Location = new System.Drawing.Point(213, 288);
+            this.edjucation.Location = new System.Drawing.Point(213, 271);
             this.edjucation.Name = "edjucation";
             this.edjucation.Size = new System.Drawing.Size(51, 15);
             this.edjucation.TabIndex = 19;
             this.edjucation.Text = "Vzdělání";
+            // 
             // sendButton
+            // 
             this.sendButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.sendButton.Location = new System.Drawing.Point(503, 354);
+            this.sendButton.Location = new System.Drawing.Point(503, 357);
             this.sendButton.Name = "sendButton";
             this.sendButton.Size = new System.Drawing.Size(75, 23);
             this.sendButton.TabIndex = 21;
             this.sendButton.Text = "Odeslat";
             this.sendButton.UseVisualStyleBackColor = true;
             this.sendButton.Click += new System.EventHandler(this.sendButton_Click_1);
+            // 
             // textBoxReligion
+            // 
             this.textBoxReligion.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBoxReligion.Location = new System.Drawing.Point(270, 137);
+            this.textBoxReligion.Location = new System.Drawing.Point(270, 123);
             this.textBoxReligion.Name = "textBoxReligion";
             this.textBoxReligion.Size = new System.Drawing.Size(308, 23);
             this.textBoxReligion.TabIndex = 22;
+            // 
             // textBoxEdjucation
+            // 
             this.textBoxEdjucation.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.textBoxEdjucation.Location = new System.Drawing.Point(270, 285);
+            this.textBoxEdjucation.Location = new System.Drawing.Point(270, 268);
             this.textBoxEdjucation.Name = "textBoxEdjucation";
             this.textBoxEdjucation.Size = new System.Drawing.Size(308, 23);
             this.textBoxEdjucation.TabIndex = 23;
+            // 
             // checkBoxGDPR
+            // 
             this.checkBoxGDPR.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.checkBoxGDPR.AutoSize = true;
-            this.checkBoxGDPR.Location = new System.Drawing.Point(270, 332);
+            this.checkBoxGDPR.Location = new System.Drawing.Point(270, 317);
             this.checkBoxGDPR.Name = "checkBoxGDPR";
-            this.checkBoxGDPR.Size = new System.Drawing.Size(295, 19);
+            this.checkBoxGDPR.Size = new System.Drawing.Size(291, 34);
             this.checkBoxGDPR.TabIndex = 24;
-            this.checkBoxGDPR.Text = "Souhlasím s podmínkami ochrama osobních údajů\r\n";
+            this.checkBoxGDPR.Text = "Souhlasím s podmínkami ochrana osobních údajů\r\n\r\n";
             this.checkBoxGDPR.UseVisualStyleBackColor = true;
+            // 
             // linkLabelGDPR
+            // 
             this.linkLabelGDPR.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.linkLabelGDPR.AutoSize = true;
-            this.linkLabelGDPR.Location = new System.Drawing.Point(282, 354);
+            this.linkLabelGDPR.Location = new System.Drawing.Point(289, 336);
             this.linkLabelGDPR.Name = "linkLabelGDPR";
             this.linkLabelGDPR.Size = new System.Drawing.Size(37, 15);
             this.linkLabelGDPR.TabIndex = 25;
             this.linkLabelGDPR.TabStop = true;
             this.linkLabelGDPR.Text = "GDPR";
             this.linkLabelGDPR.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabelGDPR_LinkClicked);
+            // 
             // LabelEdjucation
+            // 
             this.LabelEdjucation.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.LabelEdjucation.AutoSize = true;
             this.LabelEdjucation.ForeColor = System.Drawing.Color.Olive;
-            this.LabelEdjucation.Location = new System.Drawing.Point(270, 314);
+            this.LabelEdjucation.Location = new System.Drawing.Point(270, 294);
             this.LabelEdjucation.Name = "LabelEdjucation";
             this.LabelEdjucation.Size = new System.Drawing.Size(307, 15);
             this.LabelEdjucation.TabIndex = 26;
             this.LabelEdjucation.Text = "Vyplňte prosím základní škola, střední škola, vysoká škola";
+            // 
             // UserPopulationPage
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
