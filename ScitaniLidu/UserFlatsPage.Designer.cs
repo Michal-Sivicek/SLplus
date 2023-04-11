67585a785adb9d2e62446ff04646cc6bcb838f3d
Michal-Sivicek <73881006+Michal-Sivicek@users.noreply.github.com>
2023-04-11 21:05:59 +0200

    Added form to UserFlatsPage

--- ScitaniLidu/UserFlatsPage.Designer.cs
+++ ScitaniLidu/UserFlatsPage.Designer.cs
@@ -28,12 +28,278 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxObec = new System.Windows.Forms.TextBox();
+            this.obec = new System.Windows.Forms.Label();
+            this.cisloDomu = new System.Windows.Forms.Label();
+            this.textBoxCisloDomu = new System.Windows.Forms.TextBox();
+            this.PSC = new System.Windows.Forms.Label();
+            this.textBoxPSC = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.adress = new System.Windows.Forms.Label();
+            this.obydlenostBytu = new System.Windows.Forms.Label();
+            this.textBoxObydlenostBytu = new System.Windows.Forms.TextBox();
+            this.velikostBytu = new System.Windows.Forms.Label();
+            this.textBoxVelikostBytu = new System.Windows.Forms.TextBox();
+            this.podlaziBytu = new System.Windows.Forms.Label();
+            this.textBoxPodlaziBytu = new System.Windows.Forms.TextBox();
+            this.pocetLidi = new System.Windows.Forms.Label();
+            this.textBoxPocetLidi = new System.Windows.Forms.TextBox();
+            this.buttonOdeslat = new System.Windows.Forms.Button();
+            this.checkBoxPodminky = new System.Windows.Forms.CheckBox();
+            this.GDPR = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxName.Location = new System.Drawing.Point(299, 53);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(307, 23);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jméno majitele bytu";
+            // 
+            // textBoxObec
+            // 
+            this.textBoxObec.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxObec.Location = new System.Drawing.Point(299, 82);
+            this.textBoxObec.Name = "textBoxObec";
+            this.textBoxObec.Size = new System.Drawing.Size(307, 23);
+            this.textBoxObec.TabIndex = 2;
+            // 
+            // obec
+            // 
+            this.obec.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.obec.AutoSize = true;
+            this.obec.Location = new System.Drawing.Point(258, 85);
+            this.obec.Name = "obec";
+            this.obec.Size = new System.Drawing.Size(35, 15);
+            this.obec.TabIndex = 3;
+            this.obec.Text = "Obec";
+            // 
+            // cisloDomu
+            // 
+            this.cisloDomu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.cisloDomu.AutoSize = true;
+            this.cisloDomu.Location = new System.Drawing.Point(225, 114);
+            this.cisloDomu.Name = "cisloDomu";
+            this.cisloDomu.Size = new System.Drawing.Size(68, 15);
+            this.cisloDomu.TabIndex = 4;
+            this.cisloDomu.Text = "Číslo domu";
+            // 
+            // textBoxCisloDomu
+            // 
+            this.textBoxCisloDomu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxCisloDomu.Location = new System.Drawing.Point(299, 111);
+            this.textBoxCisloDomu.Name = "textBoxCisloDomu";
+            this.textBoxCisloDomu.Size = new System.Drawing.Size(307, 23);
+            this.textBoxCisloDomu.TabIndex = 5;
+            // 
+            // PSC
+            // 
+            this.PSC.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PSC.AutoSize = true;
+            this.PSC.Location = new System.Drawing.Point(265, 177);
+            this.PSC.Name = "PSC";
+            this.PSC.Size = new System.Drawing.Size(28, 15);
+            this.PSC.TabIndex = 6;
+            this.PSC.Text = "PSČ";
+            // 
+            // textBoxPSC
+            // 
+            this.textBoxPSC.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxPSC.Location = new System.Drawing.Point(299, 169);
+            this.textBoxPSC.Name = "textBoxPSC";
+            this.textBoxPSC.Size = new System.Drawing.Size(307, 23);
+            this.textBoxPSC.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Location = new System.Drawing.Point(299, 140);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(307, 23);
+            this.textBox1.TabIndex = 8;
+            // 
+            // adress
+            // 
+            this.adress.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.adress.AutoSize = true;
+            this.adress.Location = new System.Drawing.Point(260, 148);
+            this.adress.Name = "adress";
+            this.adress.Size = new System.Drawing.Size(33, 15);
+            this.adress.TabIndex = 9;
+            this.adress.Text = "Ulice";
+            // 
+            // obydlenostBytu
+            // 
+            this.obydlenostBytu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.obydlenostBytu.AutoSize = true;
+            this.obydlenostBytu.Location = new System.Drawing.Point(117, 201);
+            this.obydlenostBytu.Name = "obydlenostBytu";
+            this.obydlenostBytu.Size = new System.Drawing.Size(176, 15);
+            this.obydlenostBytu.TabIndex = 10;
+            this.obydlenostBytu.Text = "Obydlenost bytu (Počet pokojů)";
+            // 
+            // textBoxObydlenostBytu
+            // 
+            this.textBoxObydlenostBytu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxObydlenostBytu.Location = new System.Drawing.Point(299, 198);
+            this.textBoxObydlenostBytu.Name = "textBoxObydlenostBytu";
+            this.textBoxObydlenostBytu.Size = new System.Drawing.Size(307, 23);
+            this.textBoxObydlenostBytu.TabIndex = 11;
+            // 
+            // velikostBytu
+            // 
+            this.velikostBytu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.velikostBytu.AutoSize = true;
+            this.velikostBytu.Location = new System.Drawing.Point(219, 230);
+            this.velikostBytu.Name = "velikostBytu";
+            this.velikostBytu.Size = new System.Drawing.Size(74, 15);
+            this.velikostBytu.TabIndex = 12;
+            this.velikostBytu.Text = "Velikost bytu";
+            // 
+            // textBoxVelikostBytu
+            // 
+            this.textBoxVelikostBytu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxVelikostBytu.Location = new System.Drawing.Point(299, 227);
+            this.textBoxVelikostBytu.Name = "textBoxVelikostBytu";
+            this.textBoxVelikostBytu.Size = new System.Drawing.Size(307, 23);
+            this.textBoxVelikostBytu.TabIndex = 13;
+            // 
+            // podlaziBytu
+            // 
+            this.podlaziBytu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.podlaziBytu.AutoSize = true;
+            this.podlaziBytu.Location = new System.Drawing.Point(221, 259);
+            this.podlaziBytu.Name = "podlaziBytu";
+            this.podlaziBytu.Size = new System.Drawing.Size(72, 15);
+            this.podlaziBytu.TabIndex = 14;
+            this.podlaziBytu.Text = "Podlaží bytu";
+            // 
+            // textBoxPodlaziBytu
+            // 
+            this.textBoxPodlaziBytu.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxPodlaziBytu.Location = new System.Drawing.Point(299, 256);
+            this.textBoxPodlaziBytu.Name = "textBoxPodlaziBytu";
+            this.textBoxPodlaziBytu.Size = new System.Drawing.Size(307, 23);
+            this.textBoxPodlaziBytu.TabIndex = 15;
+            // 
+            // pocetLidi
+            // 
+            this.pocetLidi.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pocetLidi.AutoSize = true;
+            this.pocetLidi.Location = new System.Drawing.Point(162, 288);
+            this.pocetLidi.Name = "pocetLidi";
+            this.pocetLidi.Size = new System.Drawing.Size(131, 15);
+            this.pocetLidi.TabIndex = 16;
+            this.pocetLidi.Text = "Počet lidí v domácnosti";
+            // 
+            // textBoxPocetLidi
+            // 
+            this.textBoxPocetLidi.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBoxPocetLidi.Location = new System.Drawing.Point(299, 285);
+            this.textBoxPocetLidi.Name = "textBoxPocetLidi";
+            this.textBoxPocetLidi.Size = new System.Drawing.Size(307, 23);
+            this.textBoxPocetLidi.TabIndex = 17;
+            // 
+            // buttonOdeslat
+            // 
+            this.buttonOdeslat.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.buttonOdeslat.Location = new System.Drawing.Point(531, 348);
+            this.buttonOdeslat.Name = "buttonOdeslat";
+            this.buttonOdeslat.Size = new System.Drawing.Size(75, 23);
+            this.buttonOdeslat.TabIndex = 18;
+            this.buttonOdeslat.Text = "Odeslat";
+            this.buttonOdeslat.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPodminky
+            // 
+            this.checkBoxPodminky.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.checkBoxPodminky.AutoSize = true;
+            this.checkBoxPodminky.Location = new System.Drawing.Point(299, 314);
+            this.checkBoxPodminky.Name = "checkBoxPodminky";
+            this.checkBoxPodminky.Size = new System.Drawing.Size(291, 19);
+            this.checkBoxPodminky.TabIndex = 19;
+            this.checkBoxPodminky.Text = "Souhlasím s podmínkami ochrana osobních údajů";
+            this.checkBoxPodminky.UseVisualStyleBackColor = true;
+            // 
+            // GDPR
+            // 
+            this.GDPR.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.GDPR.AutoSize = true;
+            this.GDPR.Location = new System.Drawing.Point(299, 336);
+            this.GDPR.Name = "GDPR";
+            this.GDPR.Size = new System.Drawing.Size(37, 15);
+            this.GDPR.TabIndex = 20;
+            this.GDPR.TabStop = true;
+            this.GDPR.Text = "GDPR";
+            this.GDPR.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.GDPR_LinkClicked);
+            // 
+            // UserFlatsPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.GDPR);
+            this.Controls.Add(this.checkBoxPodminky);
+            this.Controls.Add(this.buttonOdeslat);
+            this.Controls.Add(this.textBoxPocetLidi);
+            this.Controls.Add(this.pocetLidi);
+            this.Controls.Add(this.textBoxPodlaziBytu);
+            this.Controls.Add(this.podlaziBytu);
+            this.Controls.Add(this.textBoxVelikostBytu);
+            this.Controls.Add(this.velikostBytu);
+            this.Controls.Add(this.textBoxObydlenostBytu);
+            this.Controls.Add(this.obydlenostBytu);
+            this.Controls.Add(this.adress);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxPSC);
+            this.Controls.Add(this.PSC);
+            this.Controls.Add(this.textBoxCisloDomu);
+            this.Controls.Add(this.cisloDomu);
+            this.Controls.Add(this.obec);
+            this.Controls.Add(this.textBoxObec);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "UserFlatsPage";
             this.Text = "UserFlatsPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private TextBox textBoxName;
+        private Label label1;
+        private TextBox textBoxObec;
+        private Label obec;
+        private Label cisloDomu;
+        private TextBox textBoxCisloDomu;
+        private Label PSC;
+        private TextBox textBoxPSC;
+        private TextBox textBox1;
+        private Label adress;
+        private Label obydlenostBytu;
+        private TextBox textBoxObydlenostBytu;
+        private Label velikostBytu;
+        private TextBox textBoxVelikostBytu;
+        private Label podlaziBytu;
+        private TextBox textBoxPodlaziBytu;
+        private Label pocetLidi;
+        private TextBox textBoxPocetLidi;
+        private Button buttonOdeslat;
+        private CheckBox checkBoxPodminky;
+        private LinkLabel GDPR;
     }
 }
--- ScitaniLidu/UserFlatsPage.cs
+++ ScitaniLidu/UserFlatsPage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,25 @@ namespace ScitaniLidu
         public UserFlatsPage()
         {
             InitializeComponent();
+            checkBoxPodminky.CheckedChanged += new EventHandler(checkBoxGDPR_CheckedChanged);
+            buttonOdeslat.Enabled = false; //tlačítko odeslat je na začátku neaktivní   
+        }
+
+        private void checkBoxGDPR_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxPodminky.Checked == true)
+            {
+                buttonOdeslat.Enabled = true; //povolí tlačítko odeslat, pokud je CheckBox označen
+            }
+            else
+            {
+                buttonOdeslat.Enabled = false; //neaktivní, pokud CheckBox není označen
+            }
+        }
+
+        private void GDPR_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo("https://www.uoou.cz/obecne-narizeni-o-ochrane-osobnich-udaju-gdpr/ds-3938/p1=3938") { UseShellExecute = true });
         }
     }
 }
